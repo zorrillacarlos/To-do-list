c8baad43968699cfcfab7120439aeec812e1e895
Carlos Zorrilla <zorrillacarlos4@gmail.com>
2022-05-23 02:37:33 -0400

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,75 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container">
+    <input
+      type="text"
+      class="form-control my-3"
+      v-model="newTask"
+      @keyup.enter="addTask"
+    />
+    <button class="btn btn-primary mx-3" @click="addTask">Agregar Tarea</button>
+    <button class="btn btn-danger" @click="deleteAll">Borrar Tareas</button>
+
+    <div class="mt-3" v-for="(item, index) in tasks" :key="index">
+      <div
+        role="alert"
+        :class="['alert', item.state ? 'alert-success' : 'alert-danger']"
+      >
+        <div class="d-flex justify-content-between align-items-center">
+          <div>{{ index + 1 }} - {{ item.name }} - {{ item.state }}</div>
+          <div>
+            <button
+              class="btn btn-success btn-sm mx-2"
+              @click="editTask(index)"
+            >
+              OK
+            </button>
+            <button class="btn btn-danger btn-sm" @click="deleteTask(index)">
+              X
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  data() {
+    return {
+      tasks: [],
+      newTask: "",
+    };
+  },
+  methods: {
+    addTask() {
+      this.tasks.push({
+        name: this.newTask,
+        state: false,
+      });
+      this.newTask = "";
+      localStorage.setItem("list-task", JSON.stringify(this.tasks));
+    },
+    editTask(index) {
+      this.tasks[index].state = true;
+      localStorage.setItem("list-task", JSON.stringify(this.tasks));
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+      localStorage.setItem("list-task", JSON.stringify(this.tasks));
+    },
+    deleteAll() {
+      this.tasks = [];
+      this.newTask = "";
+      localStorage.clear();
+    },
+  },
+  created() {
+    let dataDB = JSON.parse(localStorage.getItem("list-task"));
+    dataDB === null ? (this.tasks = []) : (this.tasks = dataDB);
+  },
+};
 </script>
+
+<style></style>
